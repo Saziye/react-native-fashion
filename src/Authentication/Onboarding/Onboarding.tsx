1317fd24ed630d996a16faf2b919345b64722ae9
Saziye <yilmazersaziye@gmail.com>
2021-07-13 00:13:07 +0300

    Boilerplate oluşturuldu

---
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {View} from "react-native";
+
+interface ComponentNameProps {}
+
+const ComponentName = () => {
+    return <View style={{flex:1, backgroundColor:"cyan"}}>
+
+    </View>
+};
+
+export default ComponentName;
