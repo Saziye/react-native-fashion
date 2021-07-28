51d01f09c36facce8da519daca1cbf6e43d11c9d
Saziye <yilmazersaziye@gmail.com>
2021-07-28 23:00:37 +0300

    Added animated in dot component

--- src/Authentication/Onboarding/Dot.tsx
+++ src/Authentication/Onboarding/Dot.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet } from "react-native";
-import Animated from "react-native-reanimated";
+import Animated, { Extrapolate, interpolate } from "react-native-reanimated";
 
 const styles = StyleSheet.create({
   container: {
@@ -8,6 +8,7 @@ const styles = StyleSheet.create({
     height: 8,
     width: 8,
     borderRadius: 4,
+    margin: 4,
   },
 });
 
@@ -17,7 +18,21 @@ interface DotProps {
 }
 
 const Dot = ({ index, currentIndex }: DotProps) => {
-  return <Animated.View style={styles.container}></Animated.View>;
+  const opacity = interpolate(currentIndex, {
+    inputRange: [index - 1, index, index + 1],
+    outputRange: [0.5, 1, 0.5],
+    extrapolate: Extrapolate.CLAMP,
+  });
+  const scale = interpolate(currentIndex, {
+    inputRange: [index - 1, index, index + 1],
+    outputRange: [1, 1.25, 1],
+    extrapolate: Extrapolate.CLAMP,
+  });
+  return (
+    <Animated.View
+      style={[styles.container, { opacity, transform: [{ scale }] }]}
+    ></Animated.View>
+  );
 };
 
 export default Dot;
