bd47028df40d1d8ccfd74bf3d69063485580479f
Saziye <yilmazersaziye@gmail.com>
2021-07-29 00:57:24 +0300

    Added Shopify/restyle and created theme

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-
+import {ThemeProvider} from '@shopify/restyle';
 import {Onboarding, Welcome} from "./src/Authentication";
-import { LoadAssets } from "./src/components";
+import { LoadAssets,theme } from "./src/components";
 
 const fonts = {
   "SFProText-Bold": require("./assets/fonts/SF-Pro-Text-Bold.otf"),
@@ -22,8 +22,11 @@ const AuthenticationNavigator = () => {
 
 export default function App() {
   return (
+    <ThemeProvider {...{theme}}>
+
     <LoadAssets {...{fonts}}>
       <AuthenticationNavigator />
     </LoadAssets>
+    </ThemeProvider>
   );
 }
